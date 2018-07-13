3ad38edbc661ca904e77c01395c756c8746390b0
sebastian-vn <sebastian.norena@umanizales.edu.co>
2018-07-13 18:05:07 -0500

    update

--- js/cercania_form.js
+++ js/cercania_form.js
@@ -1,15 +1,38 @@
 $(document).ready(function() {
-  getMainHeight();
+  $(".datepicker").datepicker();
+  $("select").formSelect();
   getOpacoHeight();
-  $('select').formSelect();
-
+  getSelectWidth();
+  getDateWidth();
+  selectWidth();
+  getdividerWidth();
 });
 
-function getMainHeight(){
-  $("main").css("height", $("body").height());
+function getSelectWidth() {
+  $(".select-wrapper").css("width", $("#container_form").width() / 2);
 }
 
-function getOpacoHeight(){
+function getdividerWidth(){
+  $(".divider").css("width", $("#container_form").width() - 300);
+}
+
+function getOpacoHeight() {
   $("#opaco").css("height", $("body").height() / 2);
+}
+
+function getDateWidth() {
+  $(".datepicker").css("width", $("#container_form").width() / 2);
+}
+
+//Set margin for all elements in container_form DIV
+function selectWidth() {
+  var x = document.getElementById("etj").querySelectorAll("div");
+  var i;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.marginRight = "1em";
+    x[i].style.width = "300px";
+    x[i].style.boxShadow = "0px 0px 5px 0px rgba(0,0,0,0.3)";
+    x[i].style.padding = "7px";
+  }
 }
 
